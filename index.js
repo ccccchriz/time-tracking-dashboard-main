494db4bed63f4e639cdb98190a8b53402b0eb157
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2023-11-08 17:20:09 +0100

    Selected timeframe changes color

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
+const timeframes = ["daily", "weekly", "monthly"];
+
+const categories = ["work", "play", "study", "exercise", "social", "self-care"];
+
 let data;
 
 fetch("./data.json")
   .then((res) => res.json())
   .then((json) => (data = json))
-  .then((d) => console.log(d))
-  .then(() => showData(0));
+  .then(() => showData());
 
-const showData = (timeframeIndex) => {
+const showData = (timeframeIndex = 0) => {
   if (data == undefined) {
     return;
   }
-  const timeframe = ["daily", "weekly", "monthly"][timeframeIndex];
+  const timeframe = timeframes[timeframeIndex];
 
-  const categories = [
-    "work",
-    "play",
-    "study",
-    "exercise",
-    "social",
-    "self-care",
-  ];
+  timeframes.forEach((name, index) => {
+    if (index == timeframeIndex)
+      document.getElementById(name).classList.add("dashboard__selected");
+    else document.getElementById(name).classList.remove("dashboard__selected");
+  });
 
   categories.forEach((category, index) => {
     const values = data[index].timeframes[timeframe];
@@ -29,6 +29,8 @@ const showData = (timeframeIndex) => {
   });
 };
 
-document.getElementById("daily").addEventListener("click", () => showData(0));
-document.getElementById("weekly").addEventListener("click", () => showData(1));
-document.getElementById("monthly").addEventListener("click", () => showData(2));
+timeframes.forEach((name, index) => {
+  document
+    .getElementById(name)
+    .addEventListener("click", () => showData(index));
+});
